7808c276ffd30e7455ba974ab10aa7b93dfaa759
Hiroki Araki <hiro8903@gmail.com>
2022-07-10 01:57:35 +0900

    トップページにログインフォーム作成、ログイン失敗時のエラーメッセージ表示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+
+  # トップページ兼ログインページ
   def new
+    @users = User.order(:name)
   end
+
+  def create
+    @users = User.order(:name)
+    @user = User.find(params[:session][:id]) unless params[:session][:id].blank?
+    if @user && @user.authenticate(params[:session][:password])
+
+    else
+      flash.now[:danger] = '認証に失敗しました。'
+      render :new
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'sessions#new'
   get '/signup', to:'users#new'
+
+  # ログイン機能
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
