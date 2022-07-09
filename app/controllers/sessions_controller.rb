6cfa6c2a95690212b4c7178f8516e1248e7245f5
Hiroki Araki <hiro8903@gmail.com>
2022-07-10 02:44:23 +0900

    ログイン用のメソッド作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
     @users = User.order(:name)
     @user = User.find(params[:session][:id]) unless params[:session][:id].blank?
     if @user && @user.authenticate(params[:session][:password])
-
+      log_in @user
+      redirect_to @user
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
